29c6a7c4e4a810afe7408e4a03ca411c3c4c2570
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2024-08-13 13:20:27 +0200

    feat: Seed database with initial todos
    The code changes in this commit add a new file `route.ts` under `src/app/api/seed` directory. This file contains a function `GET` that seeds the database with initial todos using Prisma. The existing todos are deleted and new todos are created with descriptions like "Learn React", "Learn Next.js", "Learn TypeScript", and "Learn Prisma". The function returns a JSON response with the message "Seed Executed".
    
    Note: This commit message follows the established convention of starting with a verb in the imperative form and providing a concise summary of the changes made.

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,20 @@
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) { 
+
+    await prisma.todo.deleteMany();
+
+    await prisma.todo.createMany({
+        data: [
+            {description: 'Learn React', complete: true,},
+            {description: 'Learn Next.js'},
+            {description: 'Learn TypeScript'},
+            {description: 'Learn Prisma'},
+        ]
+    })
+
+    
+
+  return NextResponse.json({ message: 'Seed Executed' })
+}
